aac9021ad44196f2bfef029c5f71df94d7ce8c97
parsa98 <parsaassadi@aut.ac.ir>
2018-10-05 11:37:40 +0330

    Publish file generated for Q2

--- HW1_Q2.m
+++ HW1_Q2.m
@@ -1,8 +1,16 @@
-% HW1-Q2 -  Communication systems
-% Power content of a signal is computed via two methods: 1)Sampling the
-% signal and then using trapzodial integration method 2) Creating a
-% function handler of the signal and performing integration using built-in
-% integral() function of matlab
+%% HW1-Q2 -  Communication systems
+
+
+
+%% Power content of a signal is computed via two methods:
+% 
+%
+%
+% # Sampling the signal and then using trapzodial numerical integration
+% method
+% # Creating a function handler of the squared signal and performing integration
+% using built-in integral() function of matlab
+% 
 
 % Sampling method
 t = -1 : 1e-3 : 11;
@@ -12,3 +20,7 @@ power_content_trapzodial_method = trapz(t,x.^2)
 % Function handler method
 signal_in_power_of_two = @(t) (cos(2 * pi * 47 * t) + cos(2 * pi * 219 * t)).^2;
 power_content_function_handler_method = integral(signal_in_power_of_two, 0, 10)
+
+%%
+% *As results suggest, the power content of the signal is approximately 10. The answer obtained by these two methods only differ by a narrow margin of 0.004; hence, the relative error would be 4e-4 which seems reasonable. This slight difference between answers is mainly because of the fact that integral() function and trapz() function use different numerical methods for integration, so they produce slightly different answers.*
+
